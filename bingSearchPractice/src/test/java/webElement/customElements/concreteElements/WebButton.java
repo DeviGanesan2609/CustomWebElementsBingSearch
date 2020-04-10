a10b87460ee15b294e396b9770d78c3c2077333e
DeviGanesan2609 <deviganesan26091991@gmail.com>
2020-04-10 16:35:52 +0530

    FirstCommitOfFramework

---
+++ bingSearchPractice/src/test/java/webElement/customElements/concreteElements/WebButton.java
@@ -0,0 +1,28 @@
+package webElement.customElements.concreteElements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import webElement.customElements.superElements.CustomWebElement;
+
+/**
+ * A button.
+ **/
+public class WebButton extends CustomWebElement {
+
+    /**
+     * Constructor.
+     *
+     * @param webDriver The webdriver usd to interact with the webbrowser.
+     * @param by        The locator used to identify the element(s) on the website.
+     **/
+    public WebButton(WebDriver webDriver, By by) {
+        super(webDriver, by);
+    }
+
+     /**
+     * Clicks on the button.
+     **/
+    public void click() {
+        getWebDriver().findElement(getBy()).click();
+    }
+}
